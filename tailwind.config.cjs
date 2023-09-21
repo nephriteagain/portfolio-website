ee19f708988f3e7c4d920be0cdb6e741e59ee7cf
nephriteagain <oneabove232@gmail.com>
2023-09-21 14:43:59 +0800

    color scheme change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#F1EFEF',
+				secondary: '#CCC8AA',
+				background: '#7D7C7C',
+				dark: '#191717'   
+			}
+		},
 	},
 	plugins: [],
 }
